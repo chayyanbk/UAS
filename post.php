1142920507faf16b6ed69410e48d02abe0be4e18
Bagas <chayyan.bk@gmail.com>
2021-06-28 23:43:40 +0800

    first commit

---
+++ post.php
@@ -0,0 +1,14 @@
+<?php include "header.php" ?>
+<div class="card">
+   <h2>TITLE HEADING</h2>
+   <h5>Title description, Dec 7, 2017</h5>
+   <div class="fakeimg" style="height:200px;">Image</div>
+   <p>Mdi Jatim bertambah 1.081 hari ini. Kabupaten Mojokerto menjadi daerah dengan tambahan kasus terbanyak, yakni 77 kasus.
+Dalam data yang diterima detikcom dari Satgas COVID-19 Jatim, kasus baru pada Senin (28/6/2021) yakni 77 dari Kabupaten Mojokerto, 73 dari Kota Surabaya, 67 dari Bangkalan, 55 dari Situbondo, 51 dari Sidoarjo, 50 dari Sumenep, 47 dari Magetan, 45 dari Kota Pasuruan, 44 dari Banyuwangi, 41 dari Jember, 35 dari Bondowoso, 34 dari Kota Probolinggo, 32 dari Kabupaten Pasuruan, 26 dari Kota Mojokerto dan 25 dari Kabupaten Malang.
+Lalu 25 dari Nganjuk, 22 dari Sampang, 22 dari Kota Madiun, 22 dari Kabupaten Madiun, 21 dari Pacitan, 21 dari Kabupaten Kediri, 20 dari Kota Batu, 19 dari Bojonegoro, 18 dari Lumajang, 18 dari Gresik, 17 dari Lamongan, 17 dari Ponorogo, 17 dari Kota Malang, 17 dari Ngawi, 16 dari Kabupaten Probolinggo, 16 dari Tulungagung, 15 dari Tuban, 14 dari Kota Kediri, 13 dari Kota Blitar, 10 dari Jombang, 10 dari Kabupaten Blitar, 6 dari Pamekasan dan 3 dari Trenggalek. 
+Total kasus COVID-19 di Jatim mencapai 170.765. Di mana 7.939 kasus di antaranya masih aktif atau dalam masa perawatan.
+Total pasien COVID-19 yang sembuh di Jatim mencapai 150.407. Hari ini bertambah 559. Ada 86 dari Bangkalan, 39 dari Sidoarjo, 35 dari Kota Surabaya, 33 dari Kabupaten Madiun, 31 dari Bondowoso, 27 dari Banyuwangi, 25 dari Kabupaten Malang, 24 dari Situbondo, 21 dari Jombang, 21 dari Lumajang, 20 dari Nganjuk, 19 dari Tuban, 16 dari Ponorogo, 16 dari Ngawi, 14 dari Kabupaten Pasuruan, 12 dari Sampang, 11 dari Pacitan dan 11 dari Kota Blitar.
+Lalu 11 dari Kota Madiun, 11 dari Kota Batu, 10 dari Pamekasan, 10 dari Trenggalek, 9 dari Bojonegoro, 9 dari Lamongan, 9 dari Magetan, 9 dari Gresik, 8 dari Kabupaten Kediri, 7 dari Jember, 7 dari Kabupaten Mojokerto, 7 dari Kabupaten Blitar, 5 dari Tulungagung, 4 Kota Malang, 3 dari Kabupaten Probolinggo, 2 dari Kota Pasuruan, 1 dari Kota Probolinggo dan 1 dari Kota Kediri. .</p>
+</div>
+ 
+<?php include "footer.php" ?>
