7c06d0d2eec7a2da5c05dff675e2b71cae0a734c
Bagas <chayyan.bk@gmail.com>
2021-06-28 23:46:15 +0800

    UAS

--- Indek.php
+++ Indek.php
@@ -66,3 +66,31 @@
  
 </body>
 </html>
+
+<?php include "header.php" ?>
+<div class="card">
+   <a href="post.php"><h2>TITLE HEADING</h2></a>
+   <h5>Title description, Dec 7, 2017</h5>
+   <div class="fakeimg" style="height:200px;">Image</div>
+</div>
+<div class="card">
+   <a href="post.php"><h2>TITLE HEADING</h2></a>
+   <h5>Title description, Dec 7, 2017</h5>
+   <div class="fakeimg" style="height:200px;">Image</div>
+</div>
+<div class="card">
+   <a href="post.php"><h2>TITLE HEADING</h2></a>
+   <h5>Title description, Dec 7, 2017</h5>
+   <div class="fakeimg" style="height:200px;">Image</div>
+</div>
+<div class="card">
+   <a href="post.php"><h2>TITLE HEADING</h2></a>
+   <h5>Title description, Dec 7, 2017</h5>
+   <div class="fakeimg" style="height:200px;">Image</div>
+</div>
+<div class="card">
+   <a href="post.php"><h2>TITLE HEADING</h2></a>
+   <h5>Title description, Dec 7, 2017</h5>
+   <div class="fakeimg" style="height:200px;">Image</div>
+</div>
+<?php include "footer.php" ?>
